28e2840c82835119a526ed64d2e57bc48e170a8f
DESKTOP-94RFM9M\伍鈞遠 <wu11158001@gmail.com>
2022-07-20 03:10:43 +0800

    feat: join room

--- Assets/MyScripts/StartSceneUI.cs
+++ Assets/MyScripts/StartSceneUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 /// <summary>
 /// 開始場景管理
@@ -18,20 +19,23 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     VideoPlayer videoPlayer;
 
     [Header("開始畫面")]
+    Image background_Image;//背景
     Transform startScreen;//startScreen UI控制        
     Text startTip_Text;//提示文字
     float startTip_Text_alpha;////提示文字Alpha
-    int startTipGlintControl;//閃爍控制
+    int startTipGlintControl;//閃爍控制    
 
     [Header("選擇模式畫面")]
     Transform selectModeScreen;//SelectModeScreen UI控制    
+    Transform modeSelectBackground_Image;//ModeSelectBackground_Image UI控制 
     Button modeSingle_Button;//單人模式按鈕
     Button modeConnect_Button;//連線模式按鈕
+    Text modeTip_Text;//提示文字
+    InputField nickName_InputField;//暱稱輸入框
 
     [Header("選擇腳色畫面")]
     Transform selectRoleScreen;//SelectRoleScreen UI控制
-    Button roleConfirm_Button;//腳色確定按鈕
-    Button roleBack_Button;//返回按鈕
+    Button roleConfirm_Button;//腳色確定按鈕    
     [Header("選擇腳色畫面/腳色選擇按鈕")]
     bool isSlideRoleButton;//是否滑動腳色按鈕    
     GameObject roleSelect_Button;//腳色選擇按鈕
@@ -50,16 +54,25 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     Text EquipBuff_Text;//裝備的Buff文字
     public BuffDrop buffBox_1;//Buff裝備框_1
     public BuffDrop buffBox_2;//Buff裝備框_2
+    Button roleBack_Button;//返回按鈕
 
     [Header("關卡畫面")]
     Transform levelScreen;//LevelScreen UI控制
-    Button level_1_Button;//關卡1_按鈕
+    Button level1_Button;//關卡1_按鈕
     int selectLevel;//選擇的關卡
-
-    
+    Button levelBack_Button;//返回按鈕
 
     [Header("連線模式畫面")]
-    Transform connectModeScreen;//ConnectModeScreen UI控制
+    Transform conncetModeScreen;//ConncetModScreen UI控制
+    Button createRoom_Button;//創建房間按鈕
+    Button randomRoom_Button;//隨機房間按鈕
+    Button specifyRoom_Button;//指定房間按鈕
+    InputField room_InputField;//加入房間名稱輸入框
+    Button connectModeBack_Button;//返回按鈕
+
+    [Header("連線房間畫面")]
+    Transform connectRoomScreen;//connectRoomScreen UI控制
+    Button connectRoomBack_Button;//返回按鈕
 
     void Awake()
     {
@@ -82,6 +95,7 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
         OnLevelScreenPrepare();
         OnSelectModeScreenPrepare();
         OnConnectModeScreenPrepare();
+        OnConnectRoomScreenPrepare();
     }
 
     /// <summary>
@@ -98,13 +112,13 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     /// </summary>
     void OnStartScreenPrepare()
     {
-        //影片
-        videoPlayer = Camera.main.GetComponent<VideoPlayer>();
-        videoPlayer.clip = Resources.Load<VideoClip>(loadPath.startVideo);
-
-        //開始畫面
+        
+        videoPlayer = Camera.main.GetComponent<VideoPlayer>();//影片
+        videoPlayer.clip = Resources.Load<VideoClip>(loadPath.startVideo);        
         startScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "StartScreen");//startScreen UI控制        
         startTip_Text = ExtensionMethods.FindAnyChild<Text>(transform, "StartTip_Text");//提示文字
+        background_Image = ExtensionMethods.FindAnyChild<Image>(transform, "Background_Image");//背景
+        background_Image.gameObject.SetActive(false);
 
         //畫面UI控制
         startScreen.gameObject.SetActive(false);        
@@ -115,11 +129,15 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     /// </summary>
     void OnSelectModeScreenPrepare()
     {
-        selectModeScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "SelectModeScreen");//selectModeScreen UI控制      
+        selectModeScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "SelectModeScreen");//selectModeScreen UI控制
+        modeSelectBackground_Image = ExtensionMethods.FindAnyChild<Transform>(transform, "ModeSelectBackground_Image");//ModeSelectBackground_Image UI控制                                                                                                    
         modeSingle_Button = ExtensionMethods.FindAnyChild<Button>(transform, "ModeSingle_Button");//單人模式按鈕
         modeSingle_Button.onClick.AddListener(OnIntoSelectRoleScreen);
         modeConnect_Button = ExtensionMethods.FindAnyChild<Button>(transform, "ModeConnect_Button");//連線模式按鈕
         modeConnect_Button.onClick.AddListener(OnOpenConnectModeScreen);
+        modeTip_Text = ExtensionMethods.FindAnyChild<Text>(transform, "ModeTip_Text");//提示文字
+        modeTip_Text.enabled = false;
+        nickName_InputField = ExtensionMethods.FindAnyChild<InputField>(transform, "NickName_InputField");//暱稱輸入框
 
         selectModeScreen.gameObject.SetActive(false);
     }
@@ -134,7 +152,7 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
         roleConfirm_Button = ExtensionMethods.FindAnyChild<Button>(transform, "RoleConfirm_Button");//腳色確定按鈕
         roleConfirm_Button.onClick.AddListener(OnIntoLeaveScreen);
         roleBack_Button = ExtensionMethods.FindAnyChild<Button>(transform, "RoleBack_Button");//返回按鈕
-        roleBack_Button.onClick.AddListener(OnBackSelectModeScreen);
+        roleBack_Button.onClick.AddListener(OnSelectRoleScreenBackButton);
         //選擇腳色畫面/腳色選擇按鈕
         roleSelectRight_Button = ExtensionMethods.FindAnyChild<Button>(transform, "RoleSelectRight_Button");////腳色按鈕移動(右)
         roleSelectRight_Button.onClick.AddListener(delegate { OnRoleButtonMove(direction:1); });
@@ -191,20 +209,43 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     void OnLevelScreenPrepare()
     {
         levelScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "LevelScreen");//LevelScreen UI控制 
-        level_1_Button = ExtensionMethods.FindAnyChild<Button>(transform, "Level_1_Button");//關卡1_按鈕
-        level_1_Button.onClick.AddListener(() => { OnSelectLecel(level: 1); });
+        level1_Button = ExtensionMethods.FindAnyChild<Button>(transform, "Level1_Button");//關卡1_按鈕
+        level1_Button.onClick.AddListener(() => { OnSelectLecel(level: 1); });
+        levelBack_Button = ExtensionMethods.FindAnyChild<Button>(transform, "LevelBack_Button");//返回按鈕
+        levelBack_Button.onClick.AddListener(OnLevelScreenBackButton);
 
         levelScreen.gameObject.SetActive(false);
     }   
 
     /// <summary>
-    /// 連線模式畫面籌備
+    /// 連線模式籌備
     /// </summary>
     void OnConnectModeScreenPrepare()
-    {
-        connectModeScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "ConnectModeScreen");//ConnectModeScreen UI控制
+    {        
+        conncetModeScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "ConncetModeScreen");//ConncetModScreen UI控制
+        createRoom_Button = ExtensionMethods.FindAnyChild<Button>(transform, "CreateRoom_Button");//創建房間按鈕
+        createRoom_Button.onClick.AddListener(OnCreateRoom);
+        randomRoom_Button = ExtensionMethods.FindAnyChild<Button>(transform, "RandomRoom_Button");//隨機房間按鈕
+        randomRoom_Button.onClick.AddListener(OnRandomRoom);
+        specifyRoom_Button = ExtensionMethods.FindAnyChild<Button>(transform, "SpecifyRoom_Button");//指定房間按鈕
+        specifyRoom_Button.onClick.AddListener(OnSpecifyRoom);
+        room_InputField = ExtensionMethods.FindAnyChild<InputField>(transform, "Room_InputField");//加入房間名稱輸入框
+        connectModeBack_Button = ExtensionMethods.FindAnyChild<Button>(transform, "ConnectModeBack_Button");//返回按鈕
+        connectModeBack_Button.onClick.AddListener(OnConnectModeBackButton);
 
-        connectModeScreen.gameObject.SetActive(false);
+        conncetModeScreen.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 連線房間畫面籌備
+    /// </summary>
+    void OnConnectRoomScreenPrepare()
+    {
+        connectRoomScreen = ExtensionMethods.FindAnyChild<Transform>(transform, "ConnectRoomScreen");//ConnectRoomScreen UI控制
+        connectRoomBack_Button = ExtensionMethods.FindAnyChild<Button>(transform, "ConnectRoomBack_Button");//返回按鈕
+        connectRoomBack_Button.onClick.AddListener(OnConnectRoomScreenBackButton);
+
+        connectRoomScreen.gameObject.SetActive(false);
     }
 
     void Update()
@@ -231,6 +272,7 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
             {
                 if (startScreen.gameObject.activeSelf)
                 {
+                    background_Image.gameObject.SetActive(true);
                     selectModeScreen.gameObject.SetActive(true);
                     startScreen.gameObject.SetActive(false);                    
                 }
@@ -269,17 +311,27 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     /// 開啟連線模式畫面
     /// </summary>
     void OnOpenConnectModeScreen()
-    {
+    {                
+        modeTip_Text.enabled = true;
+        modeSelectBackground_Image.gameObject.SetActive(false);
+
         PhotonNetwork.ConnectUsingSettings();//設定連線
-        connectModeScreen.gameObject.SetActive(true);
-        selectModeScreen.gameObject.SetActive(false);
     }
     /// <summary>
     /// 登入成功
     /// </summary>
     public override void OnConnectedToMaster()
     {
-        //SceneManager.LoadScene("Lobby");        
+        Debug.Log("photon連線成功");
+
+        conncetModeScreen.gameObject.SetActive(true);
+        modeSelectBackground_Image.gameObject.SetActive(true);
+        modeTip_Text.enabled = false;
+        selectModeScreen.gameObject.SetActive(false);
+
+        if (nickName_InputField.text == "") PhotonNetwork.NickName = "訪客" + Random.Range(0, 1000);
+        else PhotonNetwork.NickName = nickName_InputField.text;        
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
     #endregion
 
@@ -294,9 +346,9 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
     }
 
     /// <summary>
-    /// 返回選擇模式畫面
+    /// 選擇腳色畫面返回按鈕
     /// </summary>
-    void OnBackSelectModeScreen()
+    void OnSelectRoleScreenBackButton()
     {
         selectModeScreen.gameObject.SetActive(true);
         selectRoleScreen.gameObject.SetActive(false);
@@ -419,5 +471,83 @@ public class StartSceneUI : MonoBehaviourPunCallbacks
         levelScreen.gameObject.SetActive(false);
         StartCoroutine(LoadScene.Instance.OnLoadScene("GameScene" + selectLevel));
     }   
+
+    /// <summary>
+    /// 關卡畫面返回按鈕
+    /// </summary>
+    void OnLevelScreenBackButton()
+    {
+        selectRoleScreen.gameObject.SetActive(true);
+        levelScreen.gameObject.SetActive(false);
+    }
+    #endregion
+
+    #region 連線模式畫面   
+    /// <summary>
+    /// 返回按鈕
+    /// </summary>
+    void OnConnectModeBackButton()
+    {
+        selectModeScreen.gameObject.SetActive(true);
+        conncetModeScreen.gameObject.SetActive(false);
+        PhotonNetwork.Disconnect();
+    }
+
+    //離線觸發
+    public override void OnDisconnected(DisconnectCause cause)
+    {        
+        Debug.Log("已離線");
+    }
+    /// <summary>
+    /// 創建房間
+    /// </summary>
+    void OnCreateRoom()
+    {
+        //(防間名稱, 創建房間選擇(MaxPlayers = 最大人數), 大廳類型)
+        PhotonNetwork.CreateRoom(PhotonNetwork.NickName, new Photon.Realtime.RoomOptions { MaxPlayers = 4 }, null);
+
+        createRoom_Button.enabled = false;
+        randomRoom_Button.enabled = false;
+    }
+
+    /// <summary>
+    /// 隨機房間
+    /// </summary>
+    void OnRandomRoom()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    /// <summary>
+    /// 指定房間
+    /// </summary>
+    void OnSpecifyRoom()
+    {
+        PhotonNetwork.JoinRoom(room_InputField.text);
+    }
+
+    /// <summary>
+    /// 加入房間觸發
+    /// </summary>
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("加入" + PhotonNetwork.CurrentRoom.Name + "房間");
+
+        connectRoomScreen.gameObject.SetActive(true);
+        conncetModeScreen.gameObject.SetActive(false);
+
+    }
+    #endregion
+
+    #region 連線房間
+    /// <summary>
+    /// 連線房間返回按鈕
+    /// </summary>
+    void OnConnectRoomScreenBackButton()
+    {
+        selectModeScreen.gameObject.SetActive(true);
+        connectRoomScreen.gameObject.SetActive(false);
+        PhotonNetwork.Disconnect();
+    }
     #endregion
 }
