a2d32239f6edce18d0cc30210d1becc5fc1373de
STUDENT-PC\Student <wu11158001@gmail.com>
2022-08-04 14:34:46 +0800

    fix2: Ok

--- Assets/MyScripts/GameSceneManagement.cs
+++ Assets/MyScripts/GameSceneManagement.cs
@@ -50,7 +50,8 @@ public class GameSceneManagement : MonoBehaviourPunCallbacks
         number = objectHandle.OnCreateObject(loadPath.allPlayerCharacters[GameDataManagement.Instance.selectRoleNumber]);//產生至物件池
         objectNumber_Dictionary.Add("playerNumbering", number);//添加至紀錄中
         GameObject player = OnRequestOpenObject(OnGetObjectNumber("playerNumbering"), loadPath.allPlayerCharacters[GameDataManagement.Instance.selectRoleNumber]);//開啟物件
-        player.transform.position = new Vector3(23, 2f, 40);////設定位置
+        player.transform.position = new Vector3(190, -24, -35);//設定位置
+        player.transform.rotation = Quaternion.Euler(0, -60, 0);//設定選轉
         OnSetMiniMapPoint(player.transform, loadPath.miniMapMatirial_Player);//設定小地圖點點   
 
         //戰士物件
