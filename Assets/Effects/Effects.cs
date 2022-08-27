7a60fab53d5d8abb04c81bd2d21b878ab66be604
jonlineq <jonlineq@gmail.com>
2022-08-27 14:18:41 +0800

    fix war ach skill

--- Assets/Effects/Effects.cs
+++ Assets/Effects/Effects.cs
@@ -45,9 +45,12 @@ public class Effects : MonoBehaviour
         postProcessProfile.GetSetting<LensDistortion>().intensity.value = 0f;                //小魚眼
 
         magicNa2toWorld = gameObject.transform.parent;                                //角色的父物件，讓特效脫離角色Transform影響      
-        magicNa2 = NormalAttack_2.transform.GetChild(1);                              //要脫離的特效
+        
+        if (anim.runtimeAnimatorController.name == "2_Magician")
+        {
+            magicNa2 = NormalAttack_2.transform.GetChild(1);                              //要脫離的特效
 
-
+        }
 
         //武器發光，戰士弓箭手
         if (anim.runtimeAnimatorController.name == "1_Warrior" || anim.runtimeAnimatorController.name == "3_Archer")
