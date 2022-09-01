d47ae51e43a13dcbafb0c6f2539192a3e857e4f1
J-2021 <jonlineq@gmail.com>
2022-09-01 19:54:28 +0800

    特效，新增GuardBossNA2

--- Assets/Effects/GuardBoss/GuardBossNA2.cs
+++ Assets/Effects/GuardBoss/GuardBossNA2.cs
@@ -9,28 +9,53 @@ public class GuardBossNA2 : MonoBehaviour
     void Start()
     {
         anim = gameObject.transform.GetComponentInParent<Animator>();
-        gameObject.GetComponent<ParticleSystem>().Stop();      
+        gameObject.GetComponent<ParticleSystem>().Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
         animInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime > 0.25 && animInfo.normalizedTime < 0.65 && !gameObject.GetComponent<ParticleSystem>().isPlaying)
+
+        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime > 0.35 && animInfo.normalizedTime < 0.7 && !gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().isPlaying)
         {
-            gameObject.GetComponent<ParticleSystem>().Play();
+            gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
         }
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime >= 0.65)
+        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime > 0.35 && animInfo.normalizedTime < 0.65 && !gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().isPlaying)
         {
-            gameObject.GetComponent<ParticleSystem>().Stop();
+            gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
         }
 
+        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime >= 0.65)
+        {
+            gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().Stop();
+        }
+        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime >= 0.7)
+        {
+            gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+        }
        // TestMode();  //測試模式，用在編輯特效
     }
 
     void TestMode()
     {
         gameObject.transform.GetComponentInParent<AI>().enabled = false;
-      //  anim.Play("Attack.Attack2");
+        gameObject.transform.GetComponentInParent<CharactersCollision>().enabled = false;
+        gameObject.transform.GetComponentInParent<ConnectObject>().enabled = false;
+        gameObject.transform.GetComponentInParent<GuardBoss_Exclusive>().enabled = false;
+
+        if (!animInfo.IsName("Attack.Attack2"))
+        {
+            anim.Play("Attack.Attack2");
+
+        }
+
+
+        //         anim.Play("Stand", 0,0f);
+        //anim.speed = 0;
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    anim.speed = 1;
+        //}
     }
 }
