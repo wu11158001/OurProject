7a89a9bf52f1ca3ae81fe75481d1f99cf7829432
jonlineq <jonlineq@gmail.com>
2022-09-02 14:15:52 +0800

    特效，GuardBoss NA3

--- Assets/Effects/Enemy/GuardBoss/GuardBossNA3.cs
+++ Assets/Effects/Enemy/GuardBoss/GuardBossNA3.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GuardBossNA2 : MonoBehaviour
+public class GuardBossNA3 : MonoBehaviour
 {
     Animator anim;                 //對應角色動作組件
     AnimatorStateInfo animInfo;    //獲得動作狀態(節省腳本用)   
@@ -20,23 +20,23 @@ public class GuardBossNA2 : MonoBehaviour
     {
         animInfo = anim.GetCurrentAnimatorStateInfo(0);
 
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime > 0.35 && animInfo.normalizedTime < 0.7 && !gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().isPlaying)
+        if (animInfo.IsName("Attack.Attack3") && animInfo.normalizedTime > 0.1 && animInfo.normalizedTime < 0.2 && !gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().isPlaying)
         {
             gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
         }
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime > 0.35 && animInfo.normalizedTime < 0.65 && !gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().isPlaying)
+        if (animInfo.IsName("Attack.Attack3") && animInfo.normalizedTime > 0.1 && animInfo.normalizedTime < 0.4 && !gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().isPlaying)
         {
             gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
         }
-
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime >= 0.65)
-        {
-            gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().Stop();
-        }
-        if (animInfo.IsName("Attack.Attack2") && animInfo.normalizedTime >= 0.7)
+        if (animInfo.IsName("Attack.Attack3") && animInfo.normalizedTime >= 0.2)
         {
             gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
         }
+        if (animInfo.IsName("Attack.Attack3") && animInfo.normalizedTime >= 0.4)
+        {
+            gameObject.transform.GetChild(1).GetComponent<ParticleSystem>().Stop();
+        }
+
 
         if (testMode) TestMode(); //測試模式，用在編輯特效       
     }
@@ -48,9 +48,9 @@ public class GuardBossNA2 : MonoBehaviour
         gameObject.transform.GetComponentInParent<ConnectObject>().enabled = false;
         gameObject.transform.GetComponentInParent<GuardBoss_Exclusive>().enabled = false;
 
-        //if (!animInfo.IsName("Attack.Attack2"))
-        //{
-        //    anim.Play("Attack.Attack2");
-        //}
+        if (!animInfo.IsName("Attack.Attack3"))
+        {
+            anim.Play("Attack.Attack3");
+        }
     }
 }
