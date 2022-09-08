4091f17faef083365ddf0b04fcbfffc308d5d9d8
jonlineq <jonlineq@gmail.com>
2022-09-08 16:08:30 +0800

    燒起來

--- Assets/Effects/BossEffects.cs
+++ Assets/Effects/BossEffects.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class BossEffects : MonoBehaviour
 {
     public GameObject ragonTongue02;
+    public GameObject firePos;
+    Animator anim;                 //對應角色動作組件
+    AnimatorStateInfo animInfo;    //獲得動作狀態(節省腳本用)   
+    ParticleSystem Attack1;
     void Start()
     {
-        
+        anim = gameObject.transform.GetComponent<Animator>();                             //獲得角色動作組件         
+        Attack1 = firePos.transform.GetChild(0).GetComponent<ParticleSystem>();    //獲得特效組件;
     }
 
     
     void Update()
     {
-        
+        firePos.transform.position = ragonTongue02.transform.position;
+        firePos.transform.forward = ragonTongue02.transform.forward;
+        Fire();
+    }
+
+    void Fire()
+    {
+        var idelName = "Attack.Attack1";
+        var effect = Attack1;
+        if (animInfo.IsName(idelName) && animInfo.normalizedTime > 0.4
+                                     && animInfo.normalizedTime <=0.45 
+                                     && !effect.GetComponent<ParticleSystem>().isPlaying)
+            effect.transform.GetComponent<ParticleSystem>().Play();
     }
 }
--- Assets/MyScripts/StrongholdFire.cs
+++ Assets/MyScripts/StrongholdFire.cs
@@ -20,65 +20,89 @@ public class StrongholdFire : MonoBehaviour
     float strongholdHp;
     float strongholdMaxhp;
 
-    //void Start()
-    //{
-    //    fire1.SetActive(false);
-    //    fire2.SetActive(false);
-    //    fire3.SetActive(false);
-    //    fire4.SetActive(false);
-    //    fire5.SetActive(false);
-    //    fire6.SetActive(false);
-    //    fire7.SetActive(false);
-    //    fire8.SetActive(false);
-    //    fire9.SetActive(false);
-    //    fire10.SetActive(false);
-    //    fire11.SetActive(false);
-    //}
+    void Start()
+    {
+        fire1.SetActive(false);
+        fire2.SetActive(false);
+        fire3.SetActive(false);
+        fire4.SetActive(false);
+        fire5.SetActive(false);
+        fire6.SetActive(false);
+        fire7.SetActive(false);
+        fire8.SetActive(false);
+        fire9.SetActive(false);
+        fire10.SetActive(false);
+        fire11.SetActive(false);
+    }
 
-    //void Update()
-    //{
-    //    strongholdHp = stronghold.gameObject.GetComponent<Stronghold>().hp;
-    //    strongholdMaxhp = stronghold.gameObject.GetComponent<Stronghold>().maxHp;
+    void Update()
+    {
+        strongholdHp = stronghold.gameObject.GetComponent<Stronghold>().hp;
+        strongholdMaxhp = stronghold.gameObject.GetComponent<Stronghold>().maxHp;
 
-    //    if (strongholdHp <= strongholdMaxhp * 0.7f && strongholdHp > strongholdMaxhp * 0.35f)
-    //    {
-    //        fire1.SetActive(true);
-    //        fire2.SetActive(true);
-    //        fire3.SetActive(true);
-    //        fire4.SetActive(true);
-    //        fire5.SetActive(true);
-    //        if (!fire1.GetComponent<ParticleSystem>().isPlaying) fire1.GetComponent<ParticleSystem>().Play();
-    //        if (!fire2.GetComponent<ParticleSystem>().isPlaying) fire2.GetComponent<ParticleSystem>().Play();
-    //        if (!fire3.GetComponent<ParticleSystem>().isPlaying) fire3.GetComponent<ParticleSystem>().Play();
-    //        if (!fire4.GetComponent<ParticleSystem>().isPlaying) fire4.GetComponent<ParticleSystem>().Play();
-    //        if (!fire5.GetComponent<ParticleSystem>().isPlaying) fire5.GetComponent<ParticleSystem>().Play();
-    //        if (!fire6.GetComponent<ParticleSystem>().isPlaying) fire6.GetComponent<ParticleSystem>().Play();
-    //    }
-    //    if (strongholdHp <= strongholdMaxhp * 0.35f && strongholdHp > strongholdMaxhp * 0.1f)
-    //    {
-    //        fire6.SetActive(true);
-    //        fire7.SetActive(true);
-    //        fire8.SetActive(true);
-    //        fire9.SetActive(true);
-    //        fire10.SetActive(true);
-    //        fire11.SetActive(true);
-    //        if (!fire6.GetComponent<ParticleSystem>().isPlaying) fire6.GetComponent<ParticleSystem>().Play();
-    //        if (!fire7.GetComponent<ParticleSystem>().isPlaying) fire7.GetComponent<ParticleSystem>().Play();
-    //        if (!fire8.GetComponent<ParticleSystem>().isPlaying) fire8.GetComponent<ParticleSystem>().Play();
-    //        if (!fire9.GetComponent<ParticleSystem>().isPlaying) fire9.GetComponent<ParticleSystem>().Play();
-    //        if (!fire10.GetComponent<ParticleSystem>().isPlaying) fire10.GetComponent<ParticleSystem>().Play();
-    //        if (!fire11.GetComponent<ParticleSystem>().isPlaying) fire11.GetComponent<ParticleSystem>().Play();
-    //    }
-    //    if (strongholdHp <= strongholdMaxhp * 0.1f|| stronghold==null)
-    //    {
-    //        fire1.SetActive(false);
-    //        fire2.SetActive(false);
-    //        fire10.SetActive(false);
-    //        fire11.SetActive(false);
-    //        fire1.GetComponent<ParticleSystem>().Stop();
-    //        fire2.GetComponent<ParticleSystem>().Stop();
-    //        fire10.GetComponent<ParticleSystem>().Stop();
-    //        fire11.GetComponent<ParticleSystem>().Stop();
-    //    }
-    //}
+        if (strongholdHp <= strongholdMaxhp * 0.7f && strongholdHp > strongholdMaxhp * 0.35f)
+        {
+            fire1.SetActive(true);
+            fire2.SetActive(true);
+            fire3.SetActive(true);
+            fire4.SetActive(true);
+            fire5.SetActive(true);
+            if (!fire1.GetComponent<ParticleSystem>().isPlaying) fire1.GetComponent<ParticleSystem>().Play();
+            if (!fire2.GetComponent<ParticleSystem>().isPlaying) fire2.GetComponent<ParticleSystem>().Play();
+            if (!fire3.GetComponent<ParticleSystem>().isPlaying) fire3.GetComponent<ParticleSystem>().Play();
+            if (!fire4.GetComponent<ParticleSystem>().isPlaying) fire4.GetComponent<ParticleSystem>().Play();
+            if (!fire5.GetComponent<ParticleSystem>().isPlaying) fire5.GetComponent<ParticleSystem>().Play();
+            if (!fire6.GetComponent<ParticleSystem>().isPlaying) fire6.GetComponent<ParticleSystem>().Play();
+        }
+        if (strongholdHp <= strongholdMaxhp * 0.35f && strongholdHp > strongholdMaxhp * 0.001f)
+        {
+            fire6.SetActive(true);
+            fire7.SetActive(true);
+            fire8.SetActive(true);
+            fire9.SetActive(true);
+            fire10.SetActive(true);
+            fire11.SetActive(true);
+            if (!fire6.GetComponent<ParticleSystem>().isPlaying) fire6.GetComponent<ParticleSystem>().Play();
+            if (!fire7.GetComponent<ParticleSystem>().isPlaying) fire7.GetComponent<ParticleSystem>().Play();
+            if (!fire8.GetComponent<ParticleSystem>().isPlaying) fire8.GetComponent<ParticleSystem>().Play();
+            if (!fire9.GetComponent<ParticleSystem>().isPlaying) fire9.GetComponent<ParticleSystem>().Play();
+            if (!fire10.GetComponent<ParticleSystem>().isPlaying) fire10.GetComponent<ParticleSystem>().Play();
+            if (!fire11.GetComponent<ParticleSystem>().isPlaying) fire11.GetComponent<ParticleSystem>().Play();
+        }
+        if (strongholdHp <= strongholdMaxhp * 0.001f || stronghold == null)
+        {
+            fire1.SetActive(false);
+            fire2.SetActive(false);
+            fire3.SetActive(false);
+            fire4.SetActive(false);
+            fire5.SetActive(false);
+            fire6.SetActive(false);
+            fire7.SetActive(false);
+            fire8.SetActive(false);
+            fire9.SetActive(false);
+            fire10.SetActive(false);
+            fire11.SetActive(false);
+
+            fire1.GetComponent<ParticleSystem>().Stop();
+            fire2.GetComponent<ParticleSystem>().Stop();
+            fire3.GetComponent<ParticleSystem>().Stop();
+            fire4.GetComponent<ParticleSystem>().Stop();
+            fire5.GetComponent<ParticleSystem>().Stop();
+            fire6.GetComponent<ParticleSystem>().Stop();
+            fire7.GetComponent<ParticleSystem>().Stop();
+            fire8.GetComponent<ParticleSystem>().Stop();
+            fire9.GetComponent<ParticleSystem>().Stop();
+            fire10.GetComponent<ParticleSystem>().Stop();
+            fire11.GetComponent<ParticleSystem>().Stop();
+
+            //fire1.SetActive(false);
+            //fire2.SetActive(false);
+            //fire10.SetActive(false);
+            //fire11.SetActive(false);
+            //fire1.GetComponent<ParticleSystem>().Stop();
+            //fire2.GetComponent<ParticleSystem>().Stop();
+            //fire10.GetComponent<ParticleSystem>().Stop();
+            //fire11.GetComponent<ParticleSystem>().Stop();
+        }
+    }
 }
