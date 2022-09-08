1a4e61f847414f0955614778427d4c46245a5329
jonlineq <jonlineq@gmail.com>
2022-09-08 14:47:34 +0800

    特效，Boss攻擊，階段存檔，未完工

---
+++ Assets/Effects/BossEffects.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEffects : MonoBehaviour
+{
+    public GameObject ragonTongue02;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
