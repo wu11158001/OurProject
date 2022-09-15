a0579202a18685ce3ef8560aae8754eaa4847291
STUDENT-PC\Student <wu11158001@gmail.com>
2022-09-15 16:50:22 +0800

    deom2

--- Assets/MyScripts/CharactersCollision.cs
+++ Assets/MyScripts/CharactersCollision.cs
@@ -588,7 +588,7 @@ public class CharactersCollision : MonoBehaviourPunCallbacks
                 //Boss死亡
                 if (gameObject.layer == LayerMask.NameToLayer("Boss"))
                 {
-                    boxSize = new Vector3(boxSize.x, 2.0f, boxSize.z);
+                    boxSize = new Vector3(boxSize.x, 1.35f, boxSize.z);
                 }
 
                 //任務物件
